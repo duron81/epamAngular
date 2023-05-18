9dd65f22f2e718da6e73a263fc0e8f3c5efd7620
user <tabachnikov.andrey@gmail.com>
2023-05-18 18:18:45 +0300

    18.05.23

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LogoComponent } from './header/logo/logo.component';
+import { CoursesComponent } from './courses/courses.component';
+import { InputComponent } from './courses/input/input.component';
+import { CoursesListComponent } from './courses/courses-list/courses-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    CoursesComponent,
+    InputComponent,
+    CoursesListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
